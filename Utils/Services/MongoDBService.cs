6054772183757c6d7435743a5a46bda34b7a77e9
leo castonguay <leocast321@gmail.com>
2024-11-11 18:28:49 -0500

    Added UserServices, added folder for services, refactored MongoDbService, LoginViewModel refactor

---
+++ Utils/Services/MongoDBService.cs
@@ -0,0 +1,29 @@
+﻿using Automate.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automate.Utils.Services
+{
+    public class MongoDBService
+    {
+        private readonly IMongoDatabase _database;
+        private const string MONGO_URL = "mongodb://localhost:27017";
+        private const string DB_NAME = "AutomateDB";
+
+        public MongoDBService()
+        {
+            var client = new MongoClient(MONGO_URL);
+            _database = client.GetDatabase(DB_NAME);
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string collectionName)
+        {
+            return _database.GetCollection<T>(collectionName);
+        }
+    }
+
+}
