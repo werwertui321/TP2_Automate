a9443e12bf59796c7d76e3a1eb5eacba2f955222
leo castonguay <leocast321@gmail.com>
2024-11-14 14:53:22 -0500

    no more salt

--- Utils/DataServices/UserService.cs
+++ Utils/DataServices/UserService.cs
@@ -12,7 +12,8 @@ namespace Automate.Utils.Services
 {
     public interface IUserService
     {
-        void Authenticate(string? username, string? password);
+        User? Authenticate(string? username, string? password);
+
     }
 
     public class UserService : IUserService
@@ -20,7 +21,6 @@ namespace Automate.Utils.Services
         private readonly IMongoCollection<User> _users;
         private readonly MongoDBService _db;
         private const string COLLECTION_NAME = "Users";
-        private const string SALT = "$2a$11$egT6RJKZt3HP.sYYf/Xdw.";
 
         public UserService(MongoDBService db)
         {
@@ -28,10 +28,22 @@ namespace Automate.Utils.Services
             _users = _db.GetCollection<User>(COLLECTION_NAME);
         }
 
-        public void Authenticate(string? username, string? password)
+        public User? Authenticate(string? username, string? password)
         {
-            string hashedPassword = BC.HashPassword(password, SALT);
-            Env.authenticatedUser = _users.Find(userFromDB => userFromDB.Username == username && userFromDB.Password == hashedPassword).FirstOrDefault();
+            var filter = Builders<User>.Filter.Eq("Username", username);
+            User user = _users.Find(filter).FirstOrDefault();
+
+            if (user == null)
+                return null;
+
+            if (!VerifyPassword(password, user.Password))
+            {
+                return null;
+            }
+
+            return user;
         }
+
+        private bool VerifyPassword(string? password, string? hashedPassword) => BC.Verify(password, hashedPassword);
     }
 }
--- Utils/Env.cs
+++ Utils/Env.cs
@@ -1,10 +1,5 @@
 ﻿using Automate.Models;
 using Automate.Utils.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Automate.Utils
 {
@@ -13,6 +8,6 @@ namespace Automate.Utils
         public readonly static MongoDBService mongoDBService = new MongoDBService();
         public readonly static UserService userService = new UserService(mongoDBService);
         public readonly static CalendarService calendarService = new CalendarService(mongoDBService);
-        public static User authenticatedUser;
+        public static User? authenticatedUser;
     }
 }
