b075b5d98ba63abbf56c3878803bbb0e21cb813d
leo castonguay <leocast321@gmail.com>
2024-11-14 21:53:44 -0500

    added base tests setup

---
+++ AutomateTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+
+
+namespace AutomateTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
